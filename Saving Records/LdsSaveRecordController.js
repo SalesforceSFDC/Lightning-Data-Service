0a28e25fe9ec04e6a64e92c301c7a88ae1868809
Vuk Dukic <vuk.dukic@clara.com>
2018-02-12 13:19:06 -0800

    Create LdsSaveRecordController.js

---
+++ Saving Records/LdsSaveRecordController.js
@@ -0,0 +1,15 @@
+({
+    handleSaveRecord: function(component, event, helper) {
+        component.find("recordEditor").saveRecord($A.getCallback(function(saveResult) {
+            if (saveResult.state === "SUCCESS" || saveResult.state === "DRAFT") {
+                console.log("Save completed successfully.");
+            } else if (saveResult.state === "INCOMPLETE") {
+                console.log("User is offline, device doesn't support drafts.");
+            } else if (saveResult.state === "ERROR") {
+                console.log('Problem saving record, error: ' + 
+                           JSON.stringify(saveResult.error));
+            } else {
+                console.log('Unknown problem, state: ' + saveResult.state + ', error: ' + JSON.stringify(saveResult.error));
+            }
+        }));}
+})
