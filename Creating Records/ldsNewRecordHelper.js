2ba870e2cad5b70d4208096ec04ccfe6a4bb9477
Vuk Dukic <vuk.dukic@clara.com>
2018-02-12 13:28:22 -0800

    Create ldsNewRecordHelper.js

---
+++ Creating Records/ldsNewRecordHelper.js
@@ -0,0 +1,27 @@
+/*
+This helper validates the form values.
+*/
+
+({
+    validateContactForm: function(component) {
+        var validContact = true;
+
+         // Show error messages if required fields are blank
+        var allValid = component.find('contactField').reduce(function (validFields, inputCmp) {
+            inputCmp.showHelpMessageIfInvalid();
+            return validFields && inputCmp.get('v.validity').valid;
+        }, true);
+
+        if (allValid) {
+            // Verify we have an account to attach it to
+            var account = component.get("v.newContact");
+            if($A.util.isEmpty(account)) {
+                validContact = false;
+                console.log("Quick action context doesn't have a valid account.");
+            }
+        	return(validContact);
+            
+        }  
+	}
+       
+})
