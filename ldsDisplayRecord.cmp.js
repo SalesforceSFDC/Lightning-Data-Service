d47047ad739f459173e35000ffe77b85aa51c2d7
Vuk Dukic <vuk.dukic@clara.com>
2018-02-12 12:12:16 -0800

    Update ldsDisplayRecord.cmp.js

--- ldsDisplayRecord.cmp.js
+++ ldsDisplayRecord.cmp.js
@@ -1,36 +1,44 @@
 <aura:component implements="flexipage:availableForRecordHome, force:hasRecordId"> <!--inherit recordId attribute-->
 
-<aura:attribute name="record" type="Object" 
-  description="The record object to be displayed"/>
-<aura:attribute name="simpleRecord" type="Object" 
-  description="A simplified view record object to be displayed"/>
-<aura:attribute name="recordError" type="String" 
-  description="An error message bound to force:recordData"/>
+<aura:attribute name="record" type="Object" />
+<aura:attribute name="simpleRecord" type="Object" />
+<aura:attribute name="recordError" type="String" />
 
-<force:recordData aura:id="record"
+<force:recordData aura:id="recordEditor"
     layoutType="FULL"
     recordId="{!v.recordId}"
     targetError="{!v.recordError}"
     targetRecord="{!v.record}"
     targetFields ="{!v.simpleRecord}"
-    mode="VIEW"/>
-    
-<!-- Display a lightning card with details about the record -->
-<div class="Record Details"> 
-    <lightning:card iconName="standard:account" title="{!v.simpleRecord.Name}" >
-        <div class="slds-p-horizontal--small">
-            <p class="slds-text-heading--small">
-                <lightning:formattedText title="Billing City" value="{!v.simpleRecord.BillingCity}" /></p>
-            <p class="slds-text-heading--small">
-                <lightning:formattedText title="Billing State" value="{!v.simpleRecord.BillingState}" /></p>
-        </div>
-    </lightning:card>
-</div>
+    mode="EDIT" />
 
-<!-- Display Lightning Data Service errors, if any -->
-<aura:if isTrue="{!not(empty(v.recordError))}">
-    <div class="recordError">
-        {!v.recordError}</div>
-</aura:if>
+    <!-- Display a lightning card with details about the record -->
+    <div class="Record Details"> 
+        <lightning:card iconName="standard:account" title="{!v.simpleRecord.Name}" >
+            <div class="slds-p-horizontal--small">
+                <p class="slds-text-heading--small">
+                    <lightning:formattedText title="Billing State" value="{!v.simpleRecord.BillingState}" /></p>
+                <p class="slds-text-heading--small">
+                     <lightning:formattedText title="Billing City" value="{!v.simpleRecord.BillingCity}" /></p>
+            </div>
+        </lightning:card>
+    </div>
+    <br/>
 
+    <!-- Display an editing form -->
+    <div class="Record Details">
+        <lightning:card iconName="action:edit" title="Edit Account">
+            <div class="slds-p-horizontal--small">
+                <lightning:input label="Account Name" value="{!v.simpleRecord.Name}"/>
+                <br/>
+                <lightning:button label="Save Account" variant="brand" onclick="{!c.handleSaveRecord}" />
+            </div>
+        </lightning:card>
+    </div>
+
+    <!-- Display Lightning Data Service errors, if any -->
+    <aura:if isTrue="{!not(empty(v.recordError))}">
+        <div class="recordError">
+            {!v.recordError}</div>
+    </aura:if>
 </aura:component>
